9208ab99aa93cbd1bfe7aebe5d11994ed9a970ee
Bethel Ukegbu <167123864+Bukegbu@users.noreply.github.com>
2025-12-15 00:01:18 +0000

    Mount Google Drive and define dataset paths
    Added Google Drive mounting and dataset paths for JAFFE and CK datasets.

--- libraries.py
+++ libraries.py
@@ -10,3 +10,18 @@ from sklearn.neighbors import KNeighborsClassifier # KNN Classifier
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from skimage.feature import local_binary_pattern
+
+from google.colab import drive
+drive.mount('/content/drive/')
+
+DATA_PATHS = {
+    'jaffe_train': '/content/drive/MyDrive/prjct/JAFFE_dataset/train',
+    'jaffe_test': '/content/drive/MyDrive/prjct/JAFFE_dataset/test',
+    'ck_train': '/content/drive/MyDrive/prjct/CK_dataset/train',
+    'ck_test': '/content/drive/MyDrive/prjct/CK_dataset/test'
+}
+
+# Image parameters
+TARGET_SIZE = (48, 48)
+LBP_POINTS = 8
+LBP_RADIUS = 1
